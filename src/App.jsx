2636aeca7f1482f7a6be42fb48ec707191e5ffab
Ananan29 <arora.ananya404@gmail.com>
2024-08-15 00:43:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,28 @@ import { useState } from "react"
 import "./App.css"
 import Navigationbar from "./components/Navigationbar";
 import {ToastContainer,toast} from "react-toastify"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import 'react-toastify/dist/ReactToastify.css';
+import SignupPage from './adminauth/register/page';
+import LoginPage from './adminauth/login/page';
+import Addworkoutpage from './pages/addworkout/page';
+// import React from 'react';
+import { BrowserRouter, BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Contact from "./components/Contact/Contact"
 function App() {
 
 
   return (
-    <>
+    <BrowserRouter>
       <Navigationbar/>
-      <Router>
+      <Routes>
+          <Route path="/signup" element={<SignupPage/>} />
+          <Route path="/login" element={<LoginPage/>} />
+          <Route path="/addworkout" element={<Addworkoutpage/>} />
+          <Route path="/contact" element={<Contact/>} />
+
+      </Routes>
+      {/* <Router> */}
       
       {/* <Routes>
         <Route path="/" element={<>
@@ -20,8 +34,8 @@ function App() {
         <Route path="/workout/:type" element={<Page/>}/>
       </Routes> */}
       <ToastContainer/>
-    </Router>
-    </>
+    {/* </Router> */}
+    </BrowserRouter>
   )
 }
 
